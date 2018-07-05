9e5885c03a4861761865160006812e684c0099b5
code2hackon <chandanbera996@gmail.com>
2018-07-05 11:41:32 +0530

    Using MongoRepository

---
+++ src/main/java/com/example/mongoRep/MongoMainClass.java
@@ -0,0 +1,12 @@
+package com.example.mongoRep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoMainClass.class, args);
+	}
+}
